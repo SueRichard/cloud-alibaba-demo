73cb00eeec2c8903bf4b1b6c57843e85ee9daa16
HuangHao <1352570239@qq.com>
2024-02-06 15:50:06 +0800

    流控相关，热点参数相关，测试 ribbon熔断

--- order/src/main/java/com/hh/springcloud/alibaba/controller/OrderController.java
+++ order/src/main/java/com/hh/springcloud/alibaba/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.hh.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.hh.springcloud.alibaba.service.PayService;
 import com.hh.springcloud.entity.CommonResult;
 import com.hh.springcloud.entity.Payment;
@@ -27,10 +29,33 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
+    /**
+     * exceptionsToIgnore 这里设置的异常会忽略，会按照系统抛异常的方法处理。页面就是错误页面之类
+     *
+     * @param id
+     * @return
+     */
     @GetMapping("/ribbon/{id}")
+    @SentinelResource(value = "ribbon", fallback = "fallback", blockHandler = "blockHandler", exceptionsToIgnore = {IllegalArgumentException.class})
     public CommonResult<Payment> ribbon(@PathVariable("id") Long id) {
         CommonResult<Payment> res = restTemplate.getForObject(SERVICE_URL + "/pay/" + id, CommonResult.class, id);
+        if (id == 4) {
+            throw new IllegalArgumentException("id=4，非法参数异常");
+        } else if (res.getData() == null) {
+            throw new NullPointerException("空指针异常，id不存在");
+        }
         return res;
+    }
+
+    public CommonResult<Payment> fallback(Long id, Throwable tr) {
+        Payment payment = new Payment(id, "业务异常");
+        return new CommonResult<>(444, "业务兜底方法，tr =" + tr.getMessage(), payment);
+    }
+
+    public CommonResult<Payment> blockHandler(Long id, BlockException ex) {
+        Payment payment = new Payment(id, "控制台违规异常");
+        return new CommonResult<>(445, "控制台违规，ex =" + ex.getMessage(), payment);
+
     }
 
     @Autowired
--- payment/src/main/java/com/hh/springcloud/alibaba/controller/FlowLimitController.java
+++ payment/src/main/java/com/hh/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,8 +1,13 @@
 package com.hh.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author HH
@@ -14,14 +19,53 @@ import org.springframework.web.bind.annotation.RestController;
 public class FlowLimitController {
 
     @GetMapping("/testA")
-    public String testA() {
-        log.info(Thread.currentThread().getName()+"\t"+"****testA****");
+    public String testA() throws InterruptedException {
+        //用于线程阈值测试
+//        TimeUnit.MILLISECONDS.sleep(5000);
+        log.info(Thread.currentThread().getName() + "\t" + "****testA****");
         return "****testA****";
     }
 
     @GetMapping("/testB")
     public String testB() {
-        log.info(Thread.currentThread().getName()+"\t"+"****testB****");
+        log.info(Thread.currentThread().getName() + "\t" + "****testB****");
         return "****testB****";
+    }
+
+    @GetMapping("/testD")
+    public String testD() throws InterruptedException {
+//        TimeUnit.SECONDS.sleep(1);
+        int i = 1 / 0;
+        log.info(Thread.currentThread().getName() + "\t" + "****testD****");
+        return "****testD****";
+    }
+
+    /**
+     * 热点参数限流
+     * 必须使用@SentinelResource注解，不支持private方法
+     *
+     * @param p1
+     * @param p2
+     * @return
+     */
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "dealTestHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1, @RequestParam(value = "p2", required = false) String p2) {
+        log.info(Thread.currentThread().getName() + "\t" + "****testHotKey****");
+        return "****testHotKey****";
+    }
+
+    /**
+     * 降级方法
+     * 这里方法返回，参数签名需要和原方法一致，且在参数最后增加BlockException
+     * 如果没有该方法，那么会抛出com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowException: 1
+     *
+     * @param p1
+     * @param p2
+     * @param ex
+     * @return
+     */
+    public String dealTestHotKey(String p1, String p2, BlockException ex) {
+        return "****dealTestHotKey(...)****";
     }
 }
